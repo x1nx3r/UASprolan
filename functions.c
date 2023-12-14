dc408721abe3b1a53e0043baa08ed82f23ef5f57
Mega Nugraha <monmega110@gmail.com>
2023-12-14 21:08:20 +0700

    adding more funcs

--- functions.c
+++ functions.c
@@ -77,3 +77,19 @@ void readPeopleFromFile(const char *filename, struct Pasien *people, int count) 
 
     fclose(filePointer);
 }
+
+int binarySearch(const struct Mahasiswa identitas[], int size, const char key[]) {
+    int low = 0;
+    int high = size - 1;
+
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+
+        int cmp = strcmp(identitas[mid].nama, key);
+
+        if (cmp == 0) return mid;  // Element found
+        else if (cmp < 0) low = mid + 1;
+        else high = mid - 1;
+    }
+    return -1;  // Element not found
+}
